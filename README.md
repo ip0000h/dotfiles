d1f61e9e406026e9946526543514d0084de51a41
Ivan Gorbachev <ip0000h@gmail.com>
2016-03-23 10:51:53 +0300

    vundle changed;readme changed

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # Dotfiles
 ## Project information
-My examples of configuration files for console applications with installation script.
+This is my dotfiles project. There are many like it, but this one is mine.
 
 ## What's included
-- shell script for automatic install
 - [bash](https://www.gnu.org/software/bash/)(shell) configuration, aliases and useful scripts
 - [zsh](http://www.zsh.org/)(shell) configuration with [oh-my-zsh](https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
 - [htop](http://hisham.hm/htop/)(interactive process viewer) configuration
 - [mc](https://www.midnight-commander.org/)(file manager) configuration
 - [pyenv](https://github.com/yyuu/pyenv)(python version manager) configuration
 - [tmux](https://tmux.github.io/)(terminal multiplexer) configuration with [tpm](https://github.com/tmux-plugins/tpm) plugin manager
-- [vim](http://www.vim.org/)(text editor) configuration
+- [vim](http://www.vim.org/)(text editor) configuration with [vundle](https://github.com/VundleVim/Vundle.vim) plugin manager
 
 ## Installation
-Just run ./install.sh script.
+Just run:
+```bash
+sh -c "`curl -fsSL https://raw.githubusercontent.com/ip0000h/dotfiles/master/install.sh`"
+```
+
 
 ## Screenshot
 ![screenshot](screenshots/tmux.png)
+
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/ip0000h/dotfiles/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-################################
+###############################################################################
 # Start
 echo 'Starting...'
-################################
+###############################################################################
 # Bash
 echo 'Copying bash configuration files...'
 ln -fs "$(pwd)/bash/.bashrc" ~/.bashrc
 ln -fs "$(pwd)/bash/.bash_aliases" ~/.bash_aliases
 echo 'Done! Bash configuration installed.'
-################################
+###############################################################################
 # Zsh
 echo 'Installing oh-my-zsh(http://ohmyz.sh/)...'
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo 'Copying zsh configuration files...'
 ln -fs "$(pwd)/zsh/.zshrc" ~/.zshrc
 echo 'Done! Zsh configuration installed.'
-################################
+###############################################################################
 # PyEnv
 echo 'Installing pyenv application(https://github.com/yyuu/pyenv)...'
 echo '# PyEnv configuration' >> ~/.bashrc_custom
@@ -26,7 +26,7 @@ echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc_custom
 source ~/.bashrc_custom
 bash -c 'pyenv update'
 echo 'Done! PyEnv configuration installed.'
-################################
+###############################################################################
 # Tmux
 echo 'Installing TPM tmux plugin manager(https://github.com/tmux-plugins/tpm)...'
 mkdir -p ~/.tmux/plugins
@@ -35,21 +35,25 @@ echo 'Copying tmux configuration...'
 ln -fs "$(pwd)/tmux/.tmux.conf" ~/.tmux.conf
 tmux source ~/.tmux.conf
 echo 'Done! Tmux configuration installed. Run it and press `prefix` + I to complete install plugins.'
-################################
+###############################################################################
 # Vim
+echo 'Installing Vundle vim plugin manager(https://github.com/VundleVim/Vundle.vim)...'
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 echo 'Copying vim configuration...'
 ln -fs "$(pwd)/vim/.vimrc" ~/.vimrc
+echo 'Installing plugins...'
+vim +PluginInstall +qall
 echo 'Done! Vim configuration installed.'
-################################
+###############################################################################
 # MC
 echo 'Copying mc configuration...'
 ln -fs "$(pwd)/mc/ini" ~/.config/mc/ini
 ln -fs "$(pwd)/mc/panels.ini" ~/.config/mc/panels.ini
 echo 'Done! Mc configuration installed.'
-################################
+###############################################################################
 # Htop
 echo 'Copying htop configuration...'
 ln -fs "$(pwd)/htop/htoprc" ~/.config/htop/htoprc
 echo 'Done! Htop configuration installed.'
-################################
+###############################################################################
 echo 'All tasks done!'
