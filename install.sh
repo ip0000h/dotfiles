df5697a155c1e1bfcc10c6d83007c2e7a924976f
Ivan Gorbachev <ip0000h@gmail.com>
2023-05-15 20:47:41 +0200

    install script fix

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ else
     # pyenv deps
     sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
     libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-    xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+    xz-utils tk-dev libffi-dev liblzma-dev
 
     git clone git@github.com:ip0000h/dotfiles.git $dotfiles_dir
 
